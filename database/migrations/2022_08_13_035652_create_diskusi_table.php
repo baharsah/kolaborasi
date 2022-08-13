d771cf0672ba254d400fad465ce42bc5416d9008
Billa <nmauludina@gmail.com>
2022-08-13 11:06:48 +0700

    add migration diskusi dan industri

---
+++ database/migrations/2022_08_13_035652_create_diskusi_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diskusi', function (Blueprint $table) {
+            $table->id();
+            $table->text('chat');
+            $table->string('nama_user', 50);
+            $table->string('no_hp_user', 13);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('diskusi');
+    }
+};
