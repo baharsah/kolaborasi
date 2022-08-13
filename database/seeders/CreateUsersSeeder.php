14f4d01f6080fe50d5ef3a7b9d0ed0c067cef60e
Billa <nmauludina@gmail.com>
2022-08-14 00:11:00 +0700

    add seeder, fix user migration and model

---
+++ database/seeders/CreateUsersSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class CreateUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+               'name'=>'Musryanti',
+               'email'=>'Musry@polbeng.student.com',
+               'type'=>0,
+               'password'=> bcrypt('Musry123'),
+               'jabatan'=> 'Mahasiswa',
+               'nama_instansi'=> 'Politeknik Negeri Bengkalis'
+            ],
+            [
+               'name'=>'Desi Amirullah',
+               'email'=>'andes@polbeng.com',
+               'type'=> 2,
+               'password'=> bcrypt('Andes123'),
+               'jabatan'=> 'Dosen',
+               'nama_instansi'=> 'Politeknik Negeri Bengkalis'
+            ],
+            [
+               'name'=>'Industri',
+               'email'=>'user@industri.com',
+               'type'=>0,
+               'password'=> bcrypt('Industri123'),
+            ],
+            [
+                'name'=>'Industri2',
+                'email'=>'user2@industri.com',
+                'type'=>0,
+                'password'=> bcrypt('Industri123'),
+             ],
+        ];
+    
+        foreach ($users as $key => $user) {
+            User::create($user);
+        }
+    }
+}
